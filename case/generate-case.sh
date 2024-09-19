9c84eb98425786231fa183f0c15141d25acfa011
Jonathan Giles <jonathan@jonathangiles.net>
2024-09-19 17:20:18 +1200

    Locking in a bunch of improvements to the PCB, before a major redesign attempt

--- case/generate-case.sh
+++ case/generate-case.sh
@@ -3,10 +3,15 @@
 mkdir output
 
 # Asking for some configuration values from the user, or use defaults
+pcb_thickness="1.6"
 screw_insert_outer_diameter="3.2"
 screw_insert_depth="4.0"
+headroom="18.0"
 
 # Prompt the user for input
+read -p "What is the thickness of the PCB you plan to use? (Default is $pcb_thickness mm): " user_input
+actual_pcb_thickness=${user_input:-$pcb_thickness}
+
 read -p "What is the outer diameter of the screw inserts you plan to use? (Default is $screw_insert_outer_diameter mm): " user_input
 actual_screw_insert_outer_diameter=${user_input:-$screw_insert_outer_diameter}
 
@@ -20,9 +25,18 @@ turbocase --verbose ../spanet-pcb.kicad_pcb output/case.scad
 # Convert the single SCAD file to separate case and lid STL files using OpenSCAD
 echo -e "\n\nConverting SCAD file to STL files..."
 echo "Step 2 of 3: Generating Case STL"
-openscad -D render=\"case\" -D insert_M2_5_diameter=$actual_screw_insert_outer_diameter -D insert_M2_5_depth=$actual_screw_insert_depth -o output/case.stl output/case.scad
+openscad -D render=\"case\" \
+    -D pcb_thickness=$actual_pcb_thickness \
+    -D insert_M2_5_diameter=$actual_screw_insert_outer_diameter \
+    -D insert_M2_5_depth=$actual_screw_insert_depth \
+    -D headroom=$headroom \
+    -o output/case.stl output/case.scad
 
 echo -e "\n\nStep 3 of 3: Generating Lid STL"
-openscad -D render=\"lid\" -D insert_M2_5_diameter=$actual_screw_insert_outer_diameter -D insert_M2_5_depth=$actual_screw_insert_depth -o output/lid.stl output/case.scad
+openscad -D render=\"lid\" \
+    -D pcb_thickness=$actual_pcb_thickness \
+    -D insert_M2_5_diameter=$actual_screw_insert_outer_diameter \
+    -D insert_M2_5_depth=$actual_screw_insert_depth \
+    -o output/lid.stl output/case.scad
 
 echo -e "\n\nSuccess! SCAD files have been converted to STL files. Review the '/case/output' directory for the generated files."
