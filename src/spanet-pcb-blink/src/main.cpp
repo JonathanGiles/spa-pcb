0781b43d56e6e55e322b4984c0a35f637fd940f7
Jonathan Giles <jonathan@jonathangiles.net>
2024-10-06 09:22:12 +1300

    Major revision - up to rev 1.6 now - switching to better power supply, cleaner layer, and second PCB order from JLCPCB based on this design.

--- src/spanet-pcb-blink/src/main.cpp
+++ src/spanet-pcb-blink/src/main.cpp
@@ -3,9 +3,15 @@
 // Define the GPIO pin for the LED
 const int ledPin = 14;
 
+// check the boot button is on GPIO0
+const int bootButton = 0;
+
 void setup() {
   // Initialize the LED pin as an output
   pinMode(ledPin, OUTPUT);
+
+  // Initialize the boot button as an input
+  pinMode(bootButton, INPUT_PULLUP);
   
   // Initialize serial communication at 115200 baud rate
   Serial.begin(115200);
@@ -26,6 +32,13 @@ void loop() {
   digitalWrite(ledPin, LOW);
   // Print message to serial monitor
   Serial.println("LED is OFF");
+
+  // Check if the boot button is pressed
+  if (digitalRead(bootButton) == LOW) {
+    // Print message to serial monitor
+    Serial.println("Boot button is pressed");
+  }
+
   // Wait for 1000 milliseconds
   delay(1000);
 }
